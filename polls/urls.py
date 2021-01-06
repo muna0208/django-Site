1c16d614a2593e22ca5210eff954e89663488405
안정현 <muna2020@naver.com>
2021-01-06 16:29:12 +0900

    튜토리얼 완료

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('page', views.page, name='page')
+    path('', views.IndexView.as_view(), name='index'), # /polls/
+    path('page', views.page, name='page'), #/polls/page
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # /polls/5
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'), # /polls/5/results
+    path('<int:question_id>/vote/', views.vote, name='vote')  # /polls/5/vote
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("안녕 장고야~~~")
+from .models import Question, Choice
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list' : latest_question_list
+#     }
+#     return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question':question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question':question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    # GET POST 분기
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html',{
+                'question':question,
+                'error_message': "잘못 선택했습니다."
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 def page(request):
     return HttpResponse("좋은 페이지에 들어왔다")
